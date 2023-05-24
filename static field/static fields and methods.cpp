2d8519a5655deb63507f96033a10ffbabc022d2e
Ермакова Татьяна Вадимовна <Ermak_sf21@itstep.edu.az>
2023-05-24 18:01:03 +0400

    lesson3

---
+++ static field/static fields and methods.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+class Point {
+public:
+	int x;
+	int y;
+	static int count;
+
+	Point() {
+		x = 0;
+		y = 0;
+	}
+
+};
+
+int Point::count{ 0 };
+
+int main() {
+
+	Point point;
+	point.x = 5;
+	point.y = 7;
+	point.count = 5;
+	cout << point.x << endl;
+	cout << point.y << endl;
+	cout << point.count << endl;
+}
